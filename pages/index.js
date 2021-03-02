82684d66bded7705954a78a5e2af0738fbbf2c7e
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-03-03 03:19:25 +0900

    tailwindcssの反映

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,11 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import Image from 'next/image';
+// import Image from 'next/image';
 // import Router from 'next/router';
+
+// component
+import Sns from './components/SNS';
 
 // stylesheet
 import styles from '../styles/Home.module.css';
@@ -15,30 +18,33 @@ export default function Home() {
         <title>HOME/maresuke blog</title>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
+      <header>
+        <nav className=" flex items-center justify-between flex-wrap bg-teal-500 p-6 ">
+          <div className=" w-full block flex-grow lg:flex lg:items-center lg:w-auto ">
+            <div className=" text-sm lg:flex-grow ">
+              <p className=" block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-green-500 mr-4 " ><Link href="/Introduction">Introduction</Link></p>
+              <p className=" block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-green-500 mr-4 "><Link href="/Product">Product</Link></p>
+              <p className=" block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-green-500 mr-4 "><Link href="/Kiji">Kiji</Link></p>
+              <p className=" block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-green-500 mr-4 " ><Link href="#">blog</Link></p>
+            </div>
+          </div>
+        </nav>
+      </header>
       <body>
+      {/*<nav className=" flex items-center justify-between flex-wrap bg-teal-500 p-6 ">
+      <div className=" w-full block flex-grow lg:flex lg:items-center lg:w-auto ">
+      <div className=" text-sm lg:flex-grow ">
+              <p className=" block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-green-500 mr-4 " ><Link href="/Introduction">Introduction</Link></p>
+              <p className=" block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-green-500 mr-4 "><Link href="/Product">Product</Link></p>
+              <p className=" block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-green-500 mr-4 "><Link href="/Kiji">Kiji</Link></p>
+              <p className=" block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-green-500 mr-4 " ><Link href="#">blog</Link></p>
+            </div>
+          </div>
+        </nav>*/}
         <h1>maresuke</h1>
       </body>
       <footer>
-        <div className="github">
-          <Link href="https://github.com/daikimare">GitHub</Link>
-          {/*<Image src="vercel.svg" />*/}
-        </div>
-        <div className="twitter">
-          <Link href = "https://twitter.com/fukutaso46" title = "twitter">Twitter</Link>
-          {/* */} 
-        </div>
-        <div className="qiita">
-          <Link href="https://qiita.com/daikimare">Qiita</Link>
-          {/* */}
-        </div>
-        <div className="hanteb">
-          <Link href="https://marehiro.hateblo.jp/">HatenaBlog</Link>
-          {/* */}
-        </div>
-        <div className="note">
-          <Link href="https://note.com/daikimare">note</Link>
-          {/* */}
-        </div>
+        <Sns />
       </footer>
     </div>
   )
