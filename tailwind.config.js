0f8907c46d3c27e346481aa3695d61bcd3109d53
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-03-08 21:27:46 +0900

    create original stylw

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,35 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    fontFamily: {
+      'sans':[
+        'Roboto',
+        'Helvetica',
+        'Arial',
+        'sans-serif',
+        'Futura',
+        'Holland'
+      ]
+    },
+    extend: {
+      backgroundImage: theme => ({
+        'shinjiko': "url('/public/bacground/shinjiko-img.jpg')",
+      }),
+      backgroundOpacity: {
+        '10': '0.1',
+        '20': '0.2',
+        '95': '0.95',
+      }
+    },
+    variants: {
+      backgroundOpacity:[
+        'responsive',
+        'hover',
+        'focus',
+        'active'
+      ],
+      extend: {},
+    },
+    plugins: [],
+  }
 }
