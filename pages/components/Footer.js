c396799051eb205f7bde529f71ac91ea825eb216
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-03-10 02:39:59 +0900

    changing tag  div→footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -5,16 +5,18 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 
 export default function Footer() {
   return(
-    <div>
-      <ul className=" justify-center md:flex md:justify-center">
-        <h4 >&copy;maresuke</h4>
-        <li className="border-b md:border-none mx-2"><Link href="https://github.com/daikimare" className=" block px-8 py-2 my-4 rounded ">
-          <GitHubIcon />
-        </Link></li>
-        <li className="border-b md:border-none mx-2"><Link href = "https://twitter.com/fukutaso46" className=" block px-8 py-2 my-4 rounded ">
-          <TwitterIcon />
-        </Link></li>
-      </ul>
-    </div>
+    <footer>
+      <div className=" flex justify-center ">
+        <h4 className="">&copy;maresuke</h4>
+        <ul className=" flex ">
+          <li className="border-b md:border-none mx-2"><Link href="https://github.com/daikimare" className=" block px-8 py-2 my-4 ">
+            <GitHubIcon />
+          </Link></li>
+          <li className="border-b md:border-none mx-2"><Link href = "https://twitter.com/fukutaso46" className=" block px-8 py-2 my-4 ">
+            <TwitterIcon />
+          </Link></li>
+        </ul>
+      </div>
+    </footer>
   )
 }
