b36b2aff28190ac0abcba9b99a9c07d2bc31731c
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-03-15 15:41:38 +0900

    footerの縦幅を4px増やす

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -6,8 +6,8 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 export default function Footer() {
   return(
     <footer>
-      <div className=" flex justify-center ">
-        <h4 className="">&copy;maresuke</h4>
+      <div className=" flex justify-center py-4 ">
+        <h4>&copy; maresuke</h4>
         <ul className=" flex ">
           <li className="border-b md:border-none mx-2"><Link href="https://github.com/daikimare" className=" block px-8 py-2 my-4 ">
             <GitHubIcon />
