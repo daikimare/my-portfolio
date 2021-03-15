d6c636155661e9ea6ac03806d225d7cddce4007f
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-03-15 15:46:19 +0900

    copywrightの修正と文字の色を緑に変更

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -6,8 +6,8 @@ import TwitterIcon from '@material-ui/icons/Twitter';
 export default function Footer() {
   return(
     <footer>
-      <div className=" flex justify-center py-4 ">
-        <h4>&copy; maresuke</h4>
+      <div className=" flex justify-center py-4 bg-gray-400 text-green-300 ">
+        <h4>&copy; 2021 maresuke(Daiki Fukushima)</h4>
         <ul className=" flex ">
           <li className="border-b md:border-none mx-2"><Link href="https://github.com/daikimare" className=" block px-8 py-2 my-4 ">
             <GitHubIcon />
