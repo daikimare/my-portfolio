10132f7d53a1db59652027e980fab239cc91faa3
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-03-20 00:02:03 +0900

    rootファイルを一旦初期化

--- pages/index.js
+++ pages/index.js
@@ -2,32 +2,12 @@
 import React from 'react';
 
 // component
-// import Layout from './components/Layout';
-import Layout from '../components/Layout';
+
 
 export default function Home() {
   return (
-    <Layout title="home">
-      <div>kontikuwa</div>
-    </Layout>
-    /*
-    <body>
-      <main>
-        <div className="">
-          <Image 
-            src="/background/shinjiko.jpg"
-            alt="shinjiko"
-            layout="responsive"
-            objectFit="cover"
-            quality={100}
-            height={1200}
-            width={1920}
-            className=" opacity-60 " />
-          <div className=" absolute ">
-            <h1 className=" text-center text-8xl text-gray-300 ">maresuke</h1>
-          </div>
-        </div>
-      </main>
-    </body>*/
+    <div>
+      this is home page
+    </div>
   )
 }
