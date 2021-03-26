fa206a51e26726e00e8801f85ba5081e7c7cd634
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-03-26 11:10:43 +0900

    homepageのデザイン修正

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,33 @@
 // module
-import React from 'react';
-import Image from 'next/image';
+import React from 'react'
+import Image from 'next/image'
 
 // component
-
+import Layout from '../components/Layout'
 
 export default function Home(articles) {
   return (
-    <div className=" relative ">
-      <div className=" absolute flex flex-col text-lg font-bold md:text-7xl md:font-semibold text-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
-        <p className="">welcome to</p>
-        <p className="">maresuke's portfolio</p>
+    <Layout pageTitle="Home">
+      <div className=" relative ">
+        <div className=" absolute flex flex-col text-lg font-bold md:text-7xl md:font-semibold text-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ">
+          <p className="">welcome to</p>
+          <p className="">maresuke's portfolio</p>
+        </div>
+        <Image
+            src="/background/shinjiko.jpg"
+            alt="shinjiko"
+            layout="responsive"
+            width={1920}
+            height={1200}
+            quality={100}
+            className=" opacity-60 fixed overflow-hidden " />
+
+        <div className=" md:hidden ">
+          {/* スマートフォン画面の場合ちょっとした自己紹介を表示 */}
+          <h1>hoge</h1>
+        </div>
       </div>
-      <Image
-          src="/background/shinjiko.jpg"
-          alt="shinjiko"
-          layout="responsive"
-          width={1920}
-          height={1200}
-          quality={100}
-          className=" opacity-60 fixed overflow-hidden " />
-    </div>
+    </Layout>
   )
 }
 
